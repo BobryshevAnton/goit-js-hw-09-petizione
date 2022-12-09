92f9172ee5b07d134ce542f0927b11f20649bdaf
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2022-12-09 13:41:42 +0100

    lessom three

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,68 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// import Notiflix from 'notiflix';
+
+// const form = document.querySelector('.form');
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const {
+//     elements: { delay, step, amount },
+//   } = event.currentTarget;
+
+//   for (let i = 0; i < amount.value; i++) {
+//     let position = i + 1;
+
+//     let passedStep = Number(delay.value) + Number(step.value) * i;
+//     createPromise(position, passedStep)
+//       .then(value => {
+//         Notiflix.Notify.success(value);
+//       })
+//       .catch(value => {
+//         Notiflix.Notify.failure(value);
+//       });
+//   }
+// });
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+//       if (shouldResolve) {
+//         resolve('`✅ Fulfilled promise ${position} in ${delay}ms`');
+//       } else {
+//         reject(`❌ Rejected promise ${position} in ${delay}ms`);
+//       }
+//     }, delay);
+//   });
+// }
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  for (let i = 0; i < amount.value; i += 1) {
+    let stepPass = Number(delay.value) + Number(step.value) * i;
+    let position = i + 1;
+    createPromise(position, stepPass)
+      .then(value => {
+        Notiflix.Notify.success(value);
+      })
+      .catch(value => {
+        Notiflix.Notify.success(value);
+      });
   }
+});
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve('`✅ Fulfilled promise ${position} in ${delay}ms`');
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
